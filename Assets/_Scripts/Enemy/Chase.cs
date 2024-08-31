5c9a30adae241d9ccdf553d259f90ad055a7432a
BananaDude508 <malsebhal@gmail.com>
2024-08-31 15:22:49 +0930

    Update Chase.cs
    Removed Start() function, distance variable and normalization of direction

--- Assets/_Scripts/Enemy/Chase.cs
+++ Assets/_Scripts/Enemy/Chase.cs
@@ -8,20 +8,9 @@ public class Chase : MonoBehaviour
     public GameObject player;
     public float speed;
 
-    private float distence;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        distence = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
-        direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
