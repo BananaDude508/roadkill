dd1f9f604896d16ad3266479abd296bf658864c5
Ducksyyy <nmikellis@icloud.com>
2024-10-23 18:48:43 +1030

    reduced damage vignette and fixed it flashing

--- Assets/_Scripts/General/PostProcessing.cs
+++ Assets/_Scripts/General/PostProcessing.cs
@@ -9,12 +9,13 @@ public class PostProcessing : MonoBehaviour
 {
     public PostProcessVolume volume;
     private Vignette damageOverlay;
+    private bool beingDamaged = false;
     public float damageOverlayIntensity = 0.3f;
     public float setToZero;
     public float damagerChecker;
     public float health;
-    public float startLerp;
-    
+   
+
 
     private void Awake()
     {
@@ -22,10 +23,11 @@ public class PostProcessing : MonoBehaviour
         volume.profile.TryGetSettings(out damageOverlay);
         health = PlayerStats.playerHealth;
         damagerChecker = health;
+        
     }
     public void DamageVignette()
     {
-        
+        beingDamaged = true;
         damageOverlay.intensity.value = damageOverlayIntensity;
         StartCoroutine(DamageOverlayOff());
         damagerChecker = PlayerStats.playerHealth;
@@ -35,15 +37,17 @@ public class PostProcessing : MonoBehaviour
     void Update()
     {
         health = PlayerStats.playerHealth;
-        if (damagerChecker != health)
+        if (damagerChecker != health && !beingDamaged)
         {
             DamageVignette();
         }
+
     }
     public IEnumerator DamageOverlayOff()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
         damageOverlay.intensity.value = setToZero;
-        
+        yield return new WaitForSeconds(0.5f);
+        beingDamaged = false;
     }
 }
